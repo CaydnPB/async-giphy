56f81a685ccb504d50f8a9d5d6f1054c310621ef
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-30 15:47:44 +0000

    Add Async Function for Giphy

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImage(query) {
+    try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
+        const data = await response.json();
+        let returnURL = data.data[Math.floor(Math.random() * 25)].images.original.url
+        return returnURL;
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+(async () => {
+    try {
+        const imageUrl = await getImage("dog");
+        console.log(imageUrl);
+    } catch (error) {
+        console.error(error);
+    }
+})();
