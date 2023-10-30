64c398be7993c76c3df40bf7e3166d8db74ef66d
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-30 16:05:26 +0000

    Add Array functionality

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-async function getImage(query) {
+async function getImage(query, location) {
     try {
         const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
         const data = await response.json();
-        let returnURL = data.data[Math.floor(Math.random() * 25)].images.original.url
+        let returnURL = data.data[location].images.original.url;
         return returnURL;
     } catch (error) {
         throw new Error(error);
@@ -13,8 +13,12 @@ async function getImage(query) {
 
 (async () => {
     try {
-        const imageUrl = await getImage("dog");
-        console.log(imageUrl);
+        let allGIFs = [];
+        for (let i = 0; i < 25; i++) {
+            const imageURL = await getImage("dog", i);
+            allGIFs.push(imageURL); 
+        }
+        console.log(allGIFs);
     } catch (error) {
         console.error(error);
     }
